7096ead2241a1e2fb22f625006d46c6997906994
obara134 <tyobara@icloud.com>
2021-04-18 06:27:49 +0000

    add_item_customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'customers/show'
+  get 'customers/edit'
+  get 'items/index'
+  get 'items/show'
   devise_for :customers
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_052650) do
+ActiveRecord::Schema.define(version: 2021_04_18_061303) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2021_04_18_052650) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.integer "price"
+    t.string "image_id"
+    t.boolean "is_active", default: false, null: false  #怪しい
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
