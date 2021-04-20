76bb54aceecd2e28593b7b27fcb046ff3b194aac
tsuzuru <h.tsuzuruki@gmail.com>
2021-04-20 04:23:14 +0000

    add:アソシエーション

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,28 @@
 class Admin::ItemsController < ApplicationController
   def index
   end
-  
+
   def new
+    @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
   end
 
   def show
   end
-  
+
   def edit
   end
 
  def update
+ end
+
+ private
+
+ def item_params
+   params.require(:item).permit(:image_id, :name, :introduction, :genre_id, :price, :is_active)
  end
 
 
