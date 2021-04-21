719ed86262438bb5506c05dcaf2ee6f7695278d3
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-04-21 01:52:15 +0000

    add 会員情報編集機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,6 +12,21 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    if @customer.save
+      flash[:notice] = "You have updted customer successfully."
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
+
+    private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:kana_last_name,:kana_first_name,:postal_code,:address,:phone_number,:email,:is_deleted)
+  end
+
 
 end
