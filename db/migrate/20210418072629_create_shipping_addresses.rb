b9778542398b104105bb9a7803c32521e920081d
tsuzuru <h.tsuzuruki@gmail.com>
2021-04-22 07:24:57 +0000

    add:shiooingpage

--- db/migrate/20210418072629_create_shipping_addresses.rb
+++ db/migrate/20210418072629_create_shipping_addresses.rb
@@ -2,7 +2,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :shipping_addresses do |t|
       t.integer :customer_id
-      t.integer :name
+      t.string :name
       t.string :address
       t.string :postal_code
 
