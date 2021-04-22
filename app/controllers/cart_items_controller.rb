b85848bb098d357fd5661913741d88212fe5d61f
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-04-22 06:29:24 +0000

    add カート一覧画面　機能未実装

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.where(customer_id: current_customer.id)
   end
 
   def update
@@ -8,14 +9,24 @@ class CartItemsController < ApplicationController
   def create
     @cart_items = CartItem.new(cart_item_params)
     @cart_items.customer_id = current_customer.id
-    @cart_items.save
-    redirect_to cart_items_path
+    if
+     @cart_items.save
+     redirect_to cart_items_path
+    else
+     redirect_to request.referer
+    end
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+		@cart_item.destroy
+		redirect_to cart_items_path
   end
 
   def destroy_all
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   private
