ead215362276bd0f47eb0ccd9fbaa8ac0f9b378d
314828B <ikkunvv1101@gmail.com>
2021-04-25 09:14:09 +0000

    Add_orders_index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,20 +1,25 @@
 class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.page(params[:page]).per(10)
+    #@order_details = OrderDetail.where(order_id: @order.id)
   end
 
   def show
     @order = Order.find(params[:id])
-    #@order_details = @order.order_details
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
   def update
     @order = Order.find(params[:id])
-    # @order_details = @order.order_details
+    @order = @order.update(order_params)
+    redirect_to admin_order_path
   end
-  
+
   private
 
- 
+  def order_params
+    params.require(:order).permit(:payment_method,:postal_code,:address,:name,:shipping_cost,:total_payment,:order_status)
+  end
+
 
 end
