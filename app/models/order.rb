e7797c8b35912f9afe196d44de385a2837925c1c
tsuzuru <h.tsuzuruki@gmail.com>
2021-04-26 05:17:54 +0000

    fin

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
 
-  enum order_status: { 入金待ち: 0, 入金確認: 1, 制作中: 2, 発送準備中:3, 発送済み:4 }
+  enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中:3, 発送済み:4 }
   enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
-  
+
   belongs_to :customer
   has_many :order_details
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
 
-  enum making_status: { 制作不可: 0, 制作待ち: 1, 製作中: 2, 制作完了:3 }
+  enum making_status: { 製作不可: 0, 製作待ち: 1, 製作中: 2, 製作完了:3 }
   belongs_to :order
   belongs_to :item
 
